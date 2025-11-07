7b2089dc2ae97fbbdd17cdf90a46c59d3d5c5365
radja <radjasatrio70@gmail.com>
2025-11-07 08:25:14 +0700

    setup main.dart menggunakan material 3 dan tema berdasarkan NIM

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nim: 1123150172, Radja",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      home: const SplashScreen1(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
