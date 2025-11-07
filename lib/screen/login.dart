9ec3318b61c77120be7eddbe9a80f049e2ac426f
radja <radjasatrio70@gmail.com>
2025-11-07 09:54:10 +0700

    menambahkan icon dan text di page login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -5,6 +5,29 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.lock_person, size: 100, color: Colors.deepOrange),
+
+              SizedBox(height: 10),
+              Text(
+                "Selamat Datang, Makhluk Malam!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Silakan login untuk memulai petualangan...",
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
