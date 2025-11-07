d68ee0a10ace3727e06d7dee9a18b0af5f791537
radja <radjasatrio70@gmail.com>
2025-11-07 12:59:23 +0700

    menambahkan input Email

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -9,22 +9,47 @@ class Login extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child: const Column(
-            children: [
-              SizedBox(height: 10),
-              Icon(Icons.lock_person, size: 100, color: Colors.deepOrange),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(Icons.vpn_key, size: 100, color: Colors.deepOrange),
+                SizedBox(height: 10),
+                Text(
+                  "Selamat Datang, Makhluk Malam!",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Silakan login untuk memulai petualangan...",
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
 
-              SizedBox(height: 10),
-              Text(
-                "Selamat Datang, Makhluk Malam!",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Silakan login untuk memulai petualangan...",
-                style: TextStyle(fontSize: 14, color: Colors.grey),
-              ),
-            ],
+                SizedBox(height: 30),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Nama Lengkap',
+                    hintText: 'Masukkan nama lengkapmu',
+                    prefixIcon: const Icon(Icons.person_outline),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: Colors.blueAccent,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
