89e42cc6f74c449b14e5a08ce720326a88cd5bab
radja <radjasatrio70@gmail.com>
2025-11-07 08:56:01 +0700

    menambahkan text dibawah circle

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -15,12 +15,28 @@ class SplashScreen1 extends StatelessWidget {
               height: 160,
               decoration: const BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.deepOrange,
                 image: DecorationImage(
-                  image: AssetImage("assets/images/hw1.png"),
+                  image: AssetImage("assets/images/hw3.png"),
                   fit: BoxFit.cover,
                 ),
               ),
+            ),
+            SizedBox(height: 30),
+
+            Text(
+              "Malam Telah Tiba...",
+              style: TextStyle(
+                fontSize: 26.0, // Ukuran besar
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              "Para hantu mulai berkeliaran.\nSiap untuk trick or treat?",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14.0, color: Colors.deepOrange),
             ),
           ],
         ),
